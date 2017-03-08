ac0813a7799acf371ed4f556f1b9887450a8929a
MoisesRodriguezN <moises_rodiguez@hotmail.com>
2017-03-08 13:13:39 +0100

    Subido proyecto Angular 2

--- src/environments/firebase.config.ts
+++ src/environments/firebase.config.ts
@@ -2,11 +2,7 @@
 import {AuthMethods, AuthProviders} from "angularfire2";
 
 export const firebaseConfig = {
-    apiKey: "AIzaSyCvV5UxFB6jijPRUBcM6tlZiRVT5pPj4xM",
-    authDomain: "proyecto-angular2-moises.firebaseapp.com",
-    databaseURL: "https://proyecto-angular2-moises.firebaseio.com",
-    storageBucket: "proyecto-angular2-moises.appspot.com",
-    messagingSenderId: "502423078405"
+    
 
 };
 
